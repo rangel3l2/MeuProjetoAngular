5b894feb9d3f9c044c13e8e62ac66fba3a77fb50
rangel3l2 <rangel.silva@estudante.ifms.edu.br>
2022-09-21 11:03:43 -0400

    material/angular inserido com ul li e click event

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,  
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  constructor() { }
+  hero: Hero = {
+    id: 1,
+    name: 'Thor'
+  };
+  selectedHero?: Hero
+
+  heroes: Hero[] = [
+    {id : 1, name: 'thor'},
+    {id : 2, name: 'Homem de ferro'},
+    {id : 3, name: 'homem aranha'},
+    {id : 4, name: 'Pequena Sereia'},
+    {id : 5, name: 'Super man'}
+
+];
+selectHero(hero : Hero):void{
+this.selectedHero = hero
+
+}
+ 
+
+  ngOnInit(): void {
+  }
+
+}
